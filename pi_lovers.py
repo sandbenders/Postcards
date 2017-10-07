b60bf98a8a009263ad418855b598f6be075ceac2
sandbenders <sandbenders@riseup.net>
2017-10-07 22:03:03 +0100

    get_line function working

--- pi_lovers.py
+++ pi_lovers.py
@@ -1,15 +1,37 @@
 from pymongo import MongoClient
 import random
 
+'''
+    type:
+    male = 0
+    female = 1
+'''
+type = 0
 
-def main():
-    client = MongoClient()
-    db = client.pilovers
-    collection = db.elizabeth
+client = MongoClient()
+db = client.pilovers
+
+
+def get_line():
+    random_number = random.uniform(0, 1)
+    if type == 0:
+        if random_number < 0.5:
+            collection = db.flaubert
+        else:
+            collection = db.robert
+    else:
+        if random_number < 0.5:
+            collection = db.elizabeth
+        else:
+            collection = db.sand
 
     count = collection.count()
     line = collection.find({}, {"line": 1, "_id": 0})[random.randrange(count)]
-    print(line["line"])
+    return line['line']
+
+
+def main():
+    print(get_line())
 
 
 if __name__ == '__main__':
