a6b293af556644e9bc99ee43ee131cf1aa2619c6
sandbenders <sandbenders@riseup.net>
2017-10-10 20:02:51 +0100

    fix some PEP8 warnings

--- pi_lovers.py
+++ pi_lovers.py
@@ -1,4 +1,5 @@
-import random, sys
+import random
+import sys
 
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
--- sandbox/sandbox.py
+++ sandbox/sandbox.py
@@ -1,9 +1,9 @@
-from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 
 import time
-import traceback, sys
+import traceback
+import sys
 
 
 class WorkerSignals(QObject):
